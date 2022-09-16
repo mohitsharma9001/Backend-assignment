fcd59cc5e1571af1b20309c31437e2c3296d9059
Mohit Sharma <mohitsharma90015656@gmail.com>
2022-09-16 18:06:35 +0530

    Done

--- Day1-assignment/File-read/index.js
+++ Day1-assignment/File-read/index.js
@@ -1,14 +1,13 @@
 const fs = require("fs");
 
-// const method = process.argv[2];
-// const path = process.argv[3];
-// const content = process.argv[4];
+// Read file function
 
 const readFile = fs.readFileSync("input.txt",{
     encoding : 'utf-8'
 })
 console.log(readFile)
 
+// Append data function
 
 fs.appendFile('input.txt', 'First line added!', 'utf-8', err => {
     if (err) {
@@ -18,10 +17,19 @@ fs.appendFile('input.txt', 'First line added!', 'utf-8', err => {
     
   });
 
+  // Create file function
+
   const create = fs.createWriteStream("input.txt")
   console.log("file created")
 
+
+// rename file function
+
  const rename = fs.renameSync("input.txt","text.txt")
  console.log("File rename success")
-//   const deleate = fs.unlinkSync("input.txt")
-//   console.log("deleated file")
+
+
+ // delete file function
+
+   const deleate = fs.unlinkSync("input.txt")
+  console.log("deleated file")
