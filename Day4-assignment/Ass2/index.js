e56582d9d9e1908185d20a675700ea8403998f60
Mohit Sharma <mohitsharma90015656@gmail.com>
2022-09-22 17:01:40 +0530

    done

--- Day4-assignment/Ass2/index.js
+++ Day4-assignment/Ass2/index.js
@@ -1,16 +1,50 @@
-const express=require ("express")
-const morgan =require("morgan")
-const app=express()
-const port=3000;
+const { json } = require("express");
+const express = require("express");
+const fs = require("fs");
+const MovieRoute = express.Router()
+let count = 0;
 
-app.use(morgan("Method-:method URL-:url HTTP_Version-:http-version Status-:status Content_Length-:res[content-length] User_Agent-:user-agent Date-:date  response_time-:response-time"))
+const app = express();
+app.use(express.json())
+
+let port = 8080;
+app.listen(port, (req, res) => {
+    console.log(`listening on port ${port} `);
+});
+
+const validateMiddleware = (req, res, next) => {
+    let flg = false;
+    count++;
+    fs.readFile("movies.json", (err, data) => {
+        let movieData = req.body; // Requested movie obj
+        let serverData = JSON.parse(data); // Server movies Data
+
+        // Checking movie data avaialble or not
+        serverData.Movies.forEach(movie => {
+            if (JSON.stringify(movie) == JSON.stringify(movieData)){
+                flg = true;
+            }
+        });
+
+        if (flg) {
+            // console.log("Movie exists");
+            next();
+        }
+        else res.send(`Error 404 !`);
+    })
+
+}
+
+const getMovieData = (req, res) => {
+    let movieData = req.body
+    console.log("Visited route ", count, " times");
+
+    res.send(movieData);
+}
+
+app.use(MovieRoute);
+MovieRoute.use(validateMiddleware);
+MovieRoute.post("/movies", getMovieData);
 
 
-app.get("/",(req,res)=>{
-    res.end("Morgan logger app")
-})
 
-
-app.listen(port,()=>{
-    console.log(`server is running http://localhost:${port}`)
-})
