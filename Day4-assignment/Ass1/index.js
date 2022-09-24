89c84ce423178543bf1a4c9da4cd6bd4a0cad59c
Mohit Sharma <mohitsharma90015656@gmail.com>
2022-09-24 21:34:03 +0530

    done

--- Day4-assignment/Ass1/index.js
+++ Day4-assignment/Ass1/index.js
@@ -14,6 +14,3 @@ app.get("/",(req,res)=>{
 app.listen(port,()=>{
     console.log(`server is running http://localhost:${port}`)
 })
-app.listen(port,()=>{
-    console.log(`server is running http://localhost:${port}`)
-})
