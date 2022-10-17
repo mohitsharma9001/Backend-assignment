15c60de9f883031cd3562af5a27206947360becf
Mohit Sharma <mohitsharma90015656@gmail.com>
2022-10-17 20:58:06 +0530

    done

--- Day4-assignment/Ass1/index.js
+++ Day4-assignment/Ass1/index.js
@@ -1,17 +1,1 @@
-const express=require ("express")
-const morgan =require("morgan")
-const app=express()
-const port=3000;
-
-app.use(morgan("Method-:method URL-:url HTTP_Version-:http-version Status-:status Content_Length-:res[content-length] User_Agent-:user-agent Date-:date  response_time-:response-time"))
-
-
-app.get("/",(req,res)=>{
-    res.end("Morgan logger app")
-})
-
-
-app.listen(port,()=>{
-    console.log(`server is running http://localhost:${port}`)
-})
 
